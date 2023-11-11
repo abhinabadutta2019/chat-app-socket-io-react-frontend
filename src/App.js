cb1f25647cc35a08cff63c09661c4934e020d629
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-11-11 21:39:26 +0530

    working for two browser open, message showing from other, showin on another's browser

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import io from "socket.io-client";
 const socket = io.connect("http://localhost:3014");
 
 function App() {
+  //
+  const [message, setMessage] = useState("");
+  const [messageRecived, setMessageRecived] = useState("");
+  //
   const sendMessage = () => {
-    socket.emit("send_message", { message: "Hello gg " });
+    socket.emit("send_message", { message: message });
+    //
+    // setMessage(message);
   };
   //
   useEffect(() => {
     socket.on("recived_message", (data) => {
-      alert(data.message);
+      // alert(data.message);
+      //
+      setMessageRecived(data.message);
     });
   }, [socket]);
   return (
     <div className="App">
-      <input placeholder="message..." type="text" />
+      <input
+        placeholder="message..."
+        type="text"
+        onChange={(event) => {
+          setMessage(event.target.value);
+        }}
+      />
       <button onClick={sendMessage}>send message</button>
+      <h2>Message:</h2>
+      {messageRecived}
     </div>
   );
 }

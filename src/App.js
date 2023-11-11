1604f38cc6b4f9341dccf58cfb99a975e698525c
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-11-11 22:09:21 +0530

    room working

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import io from "socket.io-client";
 const socket = io.connect("http://localhost:3014");
 
 function App() {
+  //Room state
+
+  const [room, setRoom] = useState("");
   //
   const [message, setMessage] = useState("");
   const [messageRecived, setMessageRecived] = useState("");
   //
+  //
+  const joinRoom = () => {
+    if (room !== "") {
+      socket.emit("join_room", room);
+    }
+  };
+  //
   const sendMessage = () => {
-    socket.emit("send_message", { message: message });
+    socket.emit("send_message", { message: message, room: room });
     //
     // setMessage(message);
   };
+
   //
   useEffect(() => {
     socket.on("recived_message", (data) => {
@@ -22,6 +33,15 @@ function App() {
   }, [socket]);
   return (
     <div className="App">
+      {/*  */}
+      <input
+        placeholder="Room number..."
+        onChange={(event) => {
+          setRoom(event.target.value);
+        }}
+      />
+      <button onClick={joinRoom}>join room</button>
+      {/*  */}
       <input
         placeholder="message..."
         type="text"

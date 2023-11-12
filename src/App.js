70e98876d6eb15e321b272fec3fe8cd59e478948
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-11-12 14:26:19 +0530

    getting that message as recived message, which is sending with form submit

--- src/App.js
+++ src/App.js
@@ -3,54 +3,30 @@ import io from "socket.io-client";
 const socket = io.connect("http://localhost:3014");
 
 function App() {
-  //Room state
-
-  const [room, setRoom] = useState("");
-  //
   const [message, setMessage] = useState("");
   const [messageRecived, setMessageRecived] = useState("");
   //
-  //
-  const joinRoom = () => {
-    if (room !== "") {
-      socket.emit("join_room", room);
-    }
+  const inputMessageHandler = (event) => {
+    // console.log(e.target.value);
+    setMessage(event.target.value);
+    console.log(message, "message");
   };
   //
   const sendMessage = () => {
-    socket.emit("send_message", { message: message, room: room });
-    //
-    // setMessage(message);
+    socket.emit("send_message", { message: message });
   };
-
   //
   useEffect(() => {
     socket.on("recived_message", (data) => {
-      // alert(data.message);
-      //
       setMessageRecived(data.message);
     });
   }, [socket]);
+  //
   return (
-    <div className="App">
-      {/*  */}
-      <input
-        placeholder="Room number..."
-        onChange={(event) => {
-          setRoom(event.target.value);
-        }}
-      />
-      <button onClick={joinRoom}>join room</button>
-      {/*  */}
-      <input
-        placeholder="message..."
-        type="text"
-        onChange={(event) => {
-          setMessage(event.target.value);
-        }}
-      />
+    <div>
+      <input placeholder="message ..." onChange={inputMessageHandler} />
       <button onClick={sendMessage}>send message</button>
-      <h2>Message:</h2>
+      <h2>Message recived:</h2>
       {messageRecived}
     </div>
   );

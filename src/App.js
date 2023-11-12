d521e756fd34978248cef11794f128fed1ce32a9
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-11-12 21:27:25 +0530

    in backend console, room is showing, not in frontend, message showing in both

--- src/App.js
+++ src/App.js
@@ -4,45 +4,55 @@ const socket = io.connect("http://localhost:3014");
 
 function App() {
   const [room, setRoom] = useState("");
-  //
+  const [userName, setUserName] = useState("");
   const [message, setMessage] = useState("");
   const [messageRecived, setMessageRecived] = useState("");
-  //
+
+  const inputNameHandler = (event) => {
+    setUserName(event.target.value);
+    console.log(userName, "userName");
+  };
+
   const inputMessageHandler = (event) => {
-    // console.log(e.target.value);
     setMessage(event.target.value);
     console.log(message, "message");
   };
-  //
+
   const inputRoomHandler = (event) => {
     setRoom(event.target.value);
-    // console.log(room, "room");
+    console.log(room, "room");
   };
 
   const joinRoom = () => {
     if (room !== "") {
-      socket.emit("join_room", room);
+      socket.emit("join_room", { room: room, userName: userName });
     }
   };
 
-  //
   const sendMessage = () => {
-    socket.emit("send_message", { message: message, room: room });
+    socket.emit("send_message", {
+      message: message,
+      room: room,
+      userName: userName,
+    });
   };
-  //
+
   useEffect(() => {
     socket.on("recived_message", (data) => {
-      setMessageRecived(data.message);
+      if (data && data.message) {
+        setMessageRecived(data.message);
+      }
     });
   }, [socket]);
-  //
+
   return (
     <div>
       <input placeholder="Room..." onChange={inputRoomHandler} />
+      <input placeholder="Your Name..." onChange={inputNameHandler} />
       <button onClick={joinRoom}> Join Room</button>
-      <input placeholder="message ..." onChange={inputMessageHandler} />
-      <button onClick={sendMessage}>send message</button>
-      <h2>Message recived:</h2>
+      <input placeholder="Message..." onChange={inputMessageHandler} />
+      <button onClick={sendMessage}>Send Message</button>
+      <h2>Message received:</h2>
       {messageRecived}
     </div>
   );
